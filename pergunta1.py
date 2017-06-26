302c6cde12ae546a72095d3ebaffe71b06c8c5d3
Hextez <joao_cardoso56@hotmail.com>
2017-06-26 13:03:14 +0100

    asdasdasd

--- pergunta1.py
+++ pergunta1.py
@@ -1,14 +1,11 @@
 from pyspark import SparkContext
-from pyspark.mllib.regression import LabeledPoint
-from pyspark.mllib.feature import HashingTF
-from pyspark.mllib.classification import LogisticRegressionWithSGD
 import datetime
 from collections import Counter
 
 sc = SparkContext()
 
 #tops = sc.textFile("/home/merda/Downloads/project3-dataset.csv").map(lambda line: (str(line.split(",")[0]),long(line.split(",")[6]))).reduceByKey(lambda a , b : a+b)
-avg = sc.textFile("/home/merda/Downloads/project3-dataset.csv").map(lambda line: (str(line.split(",")[0]),{datetime.datetime.fromtimestamp(float(line.split(",")[8])).strftime('%Y-%m-%d') : long(line.split(",")[6]) })).reduceByKey(lambda a , b : dict(Counter(a)+Counter(b)))
+avg = sc.textFile("/home/cc1/project3-dataset.csv").map(lambda line: (str(line.split(",")[0]),{datetime.datetime.fromtimestamp(float(line.split(",")[8])).strftime('%Y-%m-%d') : long(line.split(",")[6]) })).reduceByKey(lambda a , b : dict(Counter(a)+Counter(b)))
 
 top = avg.map(lambda line : (line[0],sum(line[1].values())))
 avgs = avg.map(lambda line :(line[0], sum(line[1].values())/len(line[1].keys())))
--- pergunta6.py
+++ pergunta6.py
@@ -4,13 +4,24 @@ from pyspark import SparkContext
 from collections import Counter
 from pyspark.sql import SparkSession
 from pyspark.sql import *
+import datetime
+from pyspark.sql.functions import sum
 
 sc = SparkContext()
 
-#info = sc.textFile("/home/merda/Downloads/project3-dataset.csv").map(lambda line : (str(line.split(",")[0]),str(line.split(",")[1]), long(line.split(",")[6]), float(line.split(",")[8]), float(line.split(",")[10])))
-info = sc.textFile("/home/merda/Downloads/project3-dataset.csv").map(lambda line : ((str(line.split(",")[0]),str(line.split(",")[1])), long(line.split(",")[6]))).reduceByKey(lambda a, b : a + b ).map(lambda line : (line[0][0],line[0][1],line[1]))
+infos = sc.textFile("/home/cc1/project3-dataset.csv")
+source = infos.map(lambda line : (str(line.split(",")[0]), 1 ).reduceByKey(lambda a , b: a + b)
+dest = infos.map(lambda line : (str(line.split(",")[1]),1 ).reduceByKey(lambda a , b : a + b)
 
-spark = SparkSession(sc)
+total = source.union(dest).reduceByKey(lambda a , b: a + b)
+
+total.top(30,key=lambda x : x[1])
+
+ 
+#info = sc.textFile("/home/cc1/project3-dataset.csv").map(lambda line : (str(line.split(",")[0]),str(line.split(",")[1]), long(line.split(",")[6]), datetime.datetime.fromtimestamp(float(line.split(",")[8])).strftime('%Y-%m-%d'), datetime.datetime.fromtimestamp(float(line.split(",")[10])).strftime('%Y-%m-%d')))
+#info = sc.textFile("/home/cc1/project3-dataset.csv").map(lambda line : ((str(line.split(",")[0]),str(line.split(",")[1])), long(line.split(",")[6]))).reduceByKey(lambda a, b : a + b ).map(lambda line : (line[0][0],line[0][1],line[1]))
+
+#spark = SparkSession(sc)
 #sqlContext = SQLContext(spark);
 #hasattr(rdd, "toDF")
 ## True
@@ -18,16 +29,15 @@ spark = SparkSession(sc)
 #dataframe = info.toDF(['source1','dest1', 'bytes1', 'timeStart1', 'timeEnd1'])
 #dataframe2 = info.toDF(['source2','dest2', 'bytes2', 'timeStart2', 'timeEnd2'])
 
-dataframe = info.toDF(['source1','dest1', 'bytes1'])
-dataframe2 = info.toDF(['source2','dest2', 'bytes2'])
+#dataframe = info.toDF(['source1','dest1', 'bytes1'])
+#dataframe2 = info.toDF(['source2','dest2', 'bytes2'])
 
 
-data3 = dataframe.join(dataframe2, dataframe.dest1 == dataframe2.source2)
-
-data3.show()
-rdd = data3.rdd
-
-rddReduce = rdd.filter(lambda line : line[0] != line[4] and line[2] == line[5])
-
-print str(rddReduce.collect())
+#data3 = dataframe.join(dataframe2, dataframe.dest1 == dataframe2.source2)
+#dddd = data3.filter('source1 != dest2').filter('bytes1 = bytes2').filter('timeEnd1 == timeStart2').groupBy(['source1', 'dest1', 'dest2', 'timeEnd1']).agg(sum("bytes1").alias('bytes_sum'))
+#dddd.show()
+#rdd = data3.rdd
+#print str(rdd.collect())
+#rddReduce = rdd.map(lambda row : ((str(row['source1']),str(row['dest1']),str(row['dest2'])), long(row['bytes_sum'])))
+#print str(rddReduce.collect())
 
