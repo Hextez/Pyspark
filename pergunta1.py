1dd8afe6a2a13015c0cd81d10288c3e118f802d1
Hextez <joao_cardoso56@hotmail.com>
2017-06-27 12:59:35 +0100

    dasdasd

--- pergunta1.py
+++ pergunta1.py
@@ -3,22 +3,22 @@ import datetime
 from collections import Counter
 
 sc = SparkContext()
+info = sc.textFile("/home/cc1/project3-dataset.csv")
 
-avg = sc.textFile("/home/cc1/project3-dataset.csv").map(lambda line: (str(line.split(",")[0]),{datetime.datetime.fromtimestamp(float(line.split(",")[8])).strftime('%Y-%m-%d') : long(line.split(",")[6]) })).reduceByKey(lambda a , b : dict(Counter(a)+Counter(b)))
+totalInfo = info.map(lambda line: (str(line.split(",")[0]),long(line.split(",")[6]))).reduceByKey(lambda a , b : a + b)
+days = info.map(lambda line : (datetime.datetime.fromtimestamp(float(line.split(",")[8])).strftime('%Y-%m-%d') , 1 )).reduceByKey(lambda a , b : a + b)
 
-top = avg.map(lambda line : (line[0],sum(line[1].values())))
-avgs = avg.map(lambda line :(line[0], sum(line[1].values())/len(line[1].keys())))
+top = totalInfo.top(1 , key = lambda x : x[1])
+numeroDias = days.count()
 
-top_sender = top.top(1, key = lambda x: x[1])
-#top_sender = str(sorted(top.collect(),key = lambda val : val[1])[-1])
-ave_sender = avgs.top(1,key = lambda val : val[1])
-#ave_sender = str(sorted(avgs.collect(),key = lambda val : val[1])[-1])
+avg = totalInfo.map( lambda x : (x[0],x[1]/numeroDias))
+ave_sender = avg.top(1,key = lambda val : val[1])
 
 f = open("/home/cc1/OutputPergunta1.txt", "w")
-for i in top_sender:
+for i in top:
 	f.write("Quem enviou mais, " + str(i)+"\n")
 for i in ave_sender:
-	f.write("Media por dias, "+ str(i)+"\n")
+	f.write("A sua media por dias, "+ str(i)+"\n")
 f.close()
 
 
